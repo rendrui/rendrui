ed819ca63a7732d79f3ab885f87ab6af4e75e7a0
hoejsagerc <christian@hoejsager.com>
2025-12-23 19:14:28 +0100

    added base files for card component

---
+++ src/RendrUI.Components/Components/Card/CardType.cs
@@ -0,0 +1,12 @@
+namespace RendrUI.Components.Card;
+
+internal enum CardType
+{
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+    CardAction
+}
