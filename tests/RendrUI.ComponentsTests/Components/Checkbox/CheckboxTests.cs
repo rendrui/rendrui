9dceb210014a29c76403411b062bc80692ce9bd6
hoejsagerc <christian@hoejsager.com>
2026-01-08 04:55:20 +0100

    working on tests for the checkbox

--- tests/RendrUI.ComponentsTests/Components/Checkbox/CheckboxTests.cs
+++ tests/RendrUI.ComponentsTests/Components/Checkbox/CheckboxTests.cs
@@ -22,5 +22,44 @@ public class CheckboxTests : TestContextBase
         input.GetAttribute("type").ShouldBe("checkbox");
         input.HasAttribute("checked").ShouldBeFalse();
     }
+
+    [Fact]
+    public void Checkbox_ShouldApplexpectedHeighty_DefaultSize()
+    {
+        var value = false;
+
+        var cut = Render<Checkbox>(parameters => parameters
+            .Add(p => p.Value, value)
+            .Add(p => p.ValueChanged, v => value = v)
+            .Add(p => p.ValueExpression, () => value));
+
+        var input = cut.Find("input");
+
+        input.ShouldNotBeNull();
+        input.GetAttribute("class")?.ShouldContain("h-4");
+        input.GetAttribute("class")?.ShouldContain("w-4");
+    }
+
+    [Theory]
+    [InlineData(CheckboxSize.Default, "h-4", "w-4")]
+    [InlineData(CheckboxSize.Sm, "h-3", "w-3")]
+    [InlineData(CheckboxSize.Lg, "h-6", "w-6")]
+    public void Checkbox_ShouldApply_CorrectClasses_ForSize(CheckboxSize size, string expectedHeight, string expectedWidth)
+    {
+        var value = false;
+
+        var cut = Render<Checkbox>(parameters => parameters
+            .Add(p => p.Size, size)
+            .Add(p => p.Value, value)
+            .Add(p => p.ValueChanged, v => value = v)
+            .Add(p => p.ValueExpression, () => value));
+
+        var input = cut.Find("input");
+        var classAttr = input.GetAttribute("class");
+
+        classAttr.ShouldNotBeNull();
+        classAttr.ShouldContain(expectedHeight);
+        classAttr.ShouldContain(expectedWidth);
+    }
 }
 
